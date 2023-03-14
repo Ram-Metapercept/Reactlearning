df8f0ff859fe5927fc14c5f5e2478b3e01691b7e
unknown <ram.gk@metapercept.com>
2023-03-14 16:22:29 +0530

    final

--- components/StateExp.jsx
+++ components/StateExp.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 // ---------state management using hooks like useState and useEffect in functional component
-
 function StateExp() {
     // implementation of useState hook
     const [count, setCount] = useState(0)
